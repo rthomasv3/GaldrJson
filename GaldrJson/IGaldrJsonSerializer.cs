fe8a4600c1cc4546a59d0013e60f8a268972ff89
rthomasv3 <39268276+rthomasv3@users.noreply.github.com>
2025-12-15 19:55:36 -0600

    interface missing null default

--- GaldrJson/IGaldrJsonSerializer.cs
+++ GaldrJson/IGaldrJsonSerializer.cs
@@ -10,31 +10,31 @@ namespace GaldrJson
         /// <summary>
         /// Serializes the specified value to a JSON string.
         /// </summary>
-        string Serialize<T>(T value, GaldrJsonOptions options);
+        string Serialize<T>(T value, GaldrJsonOptions options = null);
 
         /// <summary>
         /// Serializes the specified value to a JSON string.
         /// </summary>
-        bool TrySerialize(object value, Type actualType, out string json, GaldrJsonOptions options);
+        bool TrySerialize(object value, Type actualType, out string json, GaldrJsonOptions options = null);
 
         /// <summary>
         /// Serializes the specified value to a JSON string.
         /// </summary>
-        bool TrySerialize<T>(T value, out string json, GaldrJsonOptions options);
+        bool TrySerialize<T>(T value, out string json, GaldrJsonOptions options = null);
 
         /// <summary>
         /// Deserializes the JSON string to the specified type.
         /// </summary>
-        T Deserialize<T>(string json, GaldrJsonOptions options);
+        T Deserialize<T>(string json, GaldrJsonOptions options = null);
 
         /// <summary>
         /// Deserializes the JSON string to the specified type.
         /// </summary>
-        bool TryDeserialize(string json, Type targetType, out object value, GaldrJsonOptions options);
+        bool TryDeserialize(string json, Type targetType, out object value, GaldrJsonOptions options = null);
 
         /// <summary>
         /// Deserializes the JSON string to the specified type.
         /// </summary>
-        bool TryDeserialize<T>(string json, out T value, GaldrJsonOptions options);
+        bool TryDeserialize<T>(string json, out T value, GaldrJsonOptions options = null);
     }
 }
